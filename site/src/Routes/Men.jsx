fb9d3c1792ec5b4912e1caf733df5da0e84a0d4b
makkardeepak25 <makkardeepak25@gmail.com>
2021-04-04 01:33:01 +0530

    deploy

---
+++ site/src/Routes/Men.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+export function Men() {
+    return (
+    <div>Men Categories</div>
+)
+      
+}
---
+++ site/src/Routes/Women.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+export function Women() {
+    return (
+    <div>Women Categories</div>
+)
+      
+}
